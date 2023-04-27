d0293732e735eb4e142411ceb1396d4bb27d585b
leafney <babycoolzx@126.com>
2023-04-27 12:15:56 +0800

    test: consumer maxInFlight

--- consumer_test.go
+++ consumer_test.go
@@ -9,25 +9,57 @@
 package rnsq
 
 import (
+	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func TestNewConsumeClient(t *testing.T) {
 
-	client := NewConsumerNSQD([]string{"127.0.0.1:4150"}, "hello", "111").SetSecret("abcd")
+	// init
+	client := NewConsumerNSQD([]string{"127.0.0.1:4150"}, "hello", "111")
+	//client := NewConsumerNSQD([]string{"127.0.0.1:4150"}, "hello", "111").SetSecret("abcd")
 
 	//client := NewConsumerNSQLookUpD([]string{""}, "", "")
+
+	// config
 	//client.SetMaxInFlight(2)
 	//client.SetSecret("abcdef")
 
-	if err := client.Consume(func(msg *XMessage) error {
+	// test Consume
+	//if err := client.Consume(func(msg *XMessage) error {
+	//	t.Log(msg.ToString())
+	//	time.Sleep(1 * time.Second)
+	//	msg.Success()
+	//	return nil
+	//}); err != nil {
+	//	log.Panicf("consume err %v", err)
+	//}
+
+	// test ConsumeMany
+	if err := client.ConsumeMany(func(msg *XMessage) error {
 		t.Log(msg.ToString())
+		time.Sleep(1 * time.Second)
 		msg.Success()
 		return nil
-	}); err != nil {
+	}, 5); err != nil {
 		log.Panicf("consume err %v", err)
 	}
+
 	defer client.Stop()
+
+	// stats and MaxInFlight
+	go func() {
+		time.Sleep(8 * time.Second)
+		fmt.Println(client.Stats())
+		fmt.Println("change maxInFlight")
+		client.ChangeMaxInFlight(0)
+		time.Sleep(5 * time.Second)
+		client.ChangeMaxInFlight(4)
+		time.Sleep(2 * time.Second)
+		client.ChangeMaxInFlight(1)
+	}()
+
 	select {}
 }
--- producer_test.go
+++ producer_test.go
@@ -11,20 +11,23 @@ package rnsq
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
 func TestNewProductClient(t *testing.T) {
 
-	client, err := NewProducer("127.0.0.1:4150", "hello", SetSecret("abcd"))
+	client, err := NewProducer("127.0.0.1:4150", "hello")
+	//client, err := NewProducer("127.0.0.1:4150", "hello",SetSecret("abcd"))
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	for i := 0; i < 10; i++ {
-		client.Publish(fmt.Sprintf("data-%v", i))
-		time.Sleep(1 * time.Second)
+	for i := 0; i < 50; i++ {
+		msg := fmt.Sprintf("data-%v", i)
+		client.Publish(msg)
+		fmt.Printf("send %v \n", msg)
+
+		//time.Sleep(1 * time.Second)
 	}
 
 }
